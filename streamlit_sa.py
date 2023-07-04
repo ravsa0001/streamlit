02cb643e37aef6b6818f3cb71053db119c015ee9
ravsa0001 <ravsaini.0011@gmail.com>
2023-07-04 19:01:01 +0530

    Commiting the streamlit file

--- streamlit_sa.py
+++ streamlit_sa.py
@@ -1,22 +1,26 @@
+# Importing libraries
 import streamlit as st
 import pickle
 
-num2label = {1 : "Good Review", 0 : "Bad Review"}
-    
-rev = st.text_area("Enter any review for your product", height = 200)
+# Taking the input or rather person review for analysys
+st.markdown("<h1 style = 'text-align : center'> Enter the review for you product </h1>", unsafe_allow_html=True)    
+rev = st.text_area("", height = 200)
 
+# Converting the string into a list
 review = [rev]
 
+# Importing the file for sentiment analysys
 with open("Notebook/model_pickle", "rb") as f:
     cv, mod = pickle.load(f)
-    
+# Performing counvectorizer that converts a string into float values
 review_count = cv.transform(review)
-
+# Analysing the review given by the user
 result = mod.predict(review_count)[0]
 
-if result == 1:
-    st.markdown("<h1 style='text-align: center; :green[Good Review. :+1:]</h1>", unsafe_allow_html=True)
-elif result == 0:
-    st.markdown("<h1 style='text-align: center; color: red;'>Bad Review :disappointed:</h1>", unsafe_allow_html=True)
-    
-st.markdown(":sunglasses:")
+# Giving the Output whether the given review is positive or not
+m=st.markdown("""<style> div.stTitle { font-size:40px; } </style>""",unsafe_allow_html=True)
+if len(rev) != 0:
+    if result == 1:
+        st.title(":green[Good Review] :smiley:")
+    elif result == 0:
+        st.title(":red[Bad Review] :disappointed: ")
