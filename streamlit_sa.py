8ebb1e1292554a3a1e71d825d208416e0ae5964e
Raghav Saini <99887889+ravsa0001@users.noreply.github.com>
2023-07-27 12:09:12 +0530

    Update the file

--- streamlit_sa.py
+++ streamlit_sa.py
@@ -1,7 +1,7 @@
 # Importing libraries
 import streamlit as st
 import pickle
-import requests
+import sklearn
 
 # Taking the input or rather person review for analysys
 st.markdown("<h1 style = 'text-align : center'> Enter the review for you product </h1>", unsafe_allow_html=True)    
@@ -10,8 +10,6 @@ rev = st.text_area("", height = 200)
 # Converting the string into a list
 review = [rev]
 
-url = "https://github.com/ravsa0001/Sentiment_Analysis/blob/main/model_pickle.pkl"
-response = requests.get(url)
 # Importing the file for sentiment analysys
 with open("model_pickle.pkl", "rb") as f:
     cv, mod = pickle.load(f)
@@ -21,7 +19,6 @@ review_count = cv.transform(review)
 result = mod.predict(review_count)[0]
 
 # Giving the Output whether the given review is positive or not
-m=st.markdown("""<style> div.stTitle { font-size:40px; } </style>""",unsafe_allow_html=True)
 if len(rev) != 0:
     if result == 1:
         st.title(":green[Good Review] :smiley:")
